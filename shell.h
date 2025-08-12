de96c7e0d96aaebc6b8b006fc386f1db3bf8e503
niimatik <niimatik.gg@gmail.com>
2025-08-12 16:26:00 +0200

    docs: add of the header file and the main file

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef shell_h
+#define shell_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif
