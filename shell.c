a1ba8d7b9c75426e0d58819c6b4b4d10260dbbdb
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 <niimatik.gg@gmail.com>
2025-08-13 10:33:14 +0200

    docs: add of the getline function and it's call in the main function

--- shell.c
+++ shell.c
@@ -1,8 +1,40 @@
 #include "shell.h"
 
+void _getline(char **command, size_t *size)
+{
+	size_t size_byte;
+
+	size_byte = getline(command, size, stdin);
+	if ((int) size_byte == EOF)
+	{
+		if (isatty(STDIN_FILENO) != 0)
+		{
+			printf("\n");
+		}
+		if (*command != NULL)
+		{
+			free(*command);
+		}
+		exit(EXIT_SUCCESS);
+	}
+}
 
 int main(int argc, char *argv[], char *envp[])
 {
+	size_t size;
+	char *command = NULL;
 
+	(void) argv;
+	while (1)
+	{
+    	size = 0;
+
+    	if (isatty(STDIN_FILENO))
+		{
+        	printf("($) ");
+		}
+		_getline(&command, &size);
+
+	}
+	return (0);
 }
-
