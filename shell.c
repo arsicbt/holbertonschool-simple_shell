7c14a7c6748ad05706ebe4a6d846054c5f359b81
niimatik <niimatik.gg@gmail.com>
2025-08-15 12:08:30 +0200

    docs: put into betty style by adding comment

--- shell.c
+++ shell.c
@@ -1,5 +1,21 @@
 #include "shell.h"
 
+/**
+ * main - Entry point of a simple shell-like program.
+ *
+ * This function runs an infinite loop where it:
+ * - Checks if the input is from an interactive terminal using isatty().
+ * - Displays a custom prompt (✿) if interactive.
+ * - Reads a command from the user using a custom _getline function.
+ *
+ * Parameters:
+ * @argc: Number of command-line arguments (unused).
+ * @argv: Array of command-line arguments (unused).
+ * @envp: Array of environment variables, passed to _getline.
+ *
+ * Return: Always returns 0.
+ */
+
 int main(int argc, char *argv[], char *envp[])
 {
 	size_t size;
@@ -7,14 +23,13 @@ int main(int argc, char *argv[], char *envp[])
 
 	(void) argc;
 	(void) argv;
-
 	size = 0;
 
 	while (1)
 	{
-    	if (isatty(STDIN_FILENO))
+		if (isatty(STDIN_FILENO))
 		{
-        	printf("✿  ");
+			printf("✿  ");
 			_getline(&command, &size, envp);
 		}
 	}
